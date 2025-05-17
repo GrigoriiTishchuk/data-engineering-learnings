bb2b9f027aad0fd956f81a878f0b4dd273d6d4e2
GregTish <tishchuk167@gmail.com>
2025-05-17 20:37:32 +0300

    addition to check *.xlsx of data adn poetry init

--- grigorii-tishchuk/dags/analysis_dag.py
+++ grigorii-tishchuk/dags/analysis_dag.py
@@ -2,10 +2,10 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-with DAG("bq_pipeline", start_date=datetime(2024, 1, 1), schedule_interval="@once", catchup=False) as dag:
+with DAG(dag_id='espoo_analysis_pipeline', start_date=datetime(2024, 1, 1), schedule_interval="@once", catchup=False,description='Espoo notebook analysis is running', ) as dag:
     run_pipeline = BashOperator(
         task_id="run_bq_load",
-        bash_command="poetry run python scripts/process_and_load.py"
+        bash_command="poetry run python scripts/run_notebook.py"
     )
 
     run_pipeline
